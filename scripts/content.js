88c518cd81534d700c0b692a122005ed4e7177a0
panhavad <panhavadd@gmail.com>
2021-04-19 01:15:11 +0700

    complete s.taobao

--- scripts/content.js
+++ scripts/content.js
@@ -1,33 +1,83 @@
 //cariable declarion
 var currency_url = "https://free.currconv.com/api/v7/convert?q=CNY_USD&compact=ultra&apiKey=6794cace11c7c0ba19d5"
 var currency_rate = 0
+var warning_header = `
+<div style="
+    padding: 5px;
+    text-align: center;
+    font-weight: bold;
+    color: blue;
+    top: 0px;
+    background-color: white;
+">You are currently using automatic price converter to USD, the USD price are not guarantee 100% accuracy. (1¥ = 0.15335 USD)</div>`
 
 //get the current CNY to USD rate
-$.get(currency_url, function( data ) {
-  $( ".result" ).html( data );
-  currency_rate = data.CNY_USD
-  //add warning letter
-  $(".oversea-head:first").after("You are currently using automatic price converter to USD, the USD price are not guarantee 100% accuracy. (1¥ = "+currency_rate+" USD)")
-  // console.log("Current rate:", currency_rate)
+$.get(currency_url, function(data) {
+    $(".result").html(data);
+    currency_rate = data.CNY_USD
+    //add warning letter
+    $("body").prepend(warning_header)
+    // console.log("Current rate:", currency_rate)
 });
 
+
+//effect only content in taobao home page
 if (location.href.includes("https://world.taobao.com/")) {
-  //searching for price
-  // console.log("Taobao USD Injected!! [world.taobao]")
+    // console.log("Taobao USD Injected!! [world.taobao]")
+    $(window).on('load', function() {
+        // console.log("[world.taobao] Loaded")
+        // console.log("Number of item"$(".price").length)
+        add_usd_price($(".price"))
+    })
 
-  $(window).on('load',function(){
-    // console.log("[world.taobao] Loaded")
-    // console.log("Number of item"$(".price").length)
-    add_usd_price($(".price"))
-  })
-
-  function add_usd_price(price_elements) {
-    // console.log("[add_usd_price] Called!!")
-    price_elements.each(function(index, each_element) {
-      original_price = parseFloat(each_element.textContent.substring(1))
-      converted_price = (original_price * currency_rate).toFixed(2)
-      each_element.textContent = each_element.textContent + " or " + converted_price + " USD"
-    });
-  }
+    function add_usd_price(price_elements) {
+        // console.log("[add_usd_price] Called!!")
+        price_elements.each(function(index, each_element) {
+            original_price = parseFloat(each_element.textContent.substring(1))
+            converted_price = (original_price * currency_rate).toFixed(2)
+            each_element.textContent = each_element.textContent + " or " + converted_price + " USD"
+        });
+    }
 }
 
+
+//effect only in taobao search page
+if (location.href.includes("https://s.taobao.com/")) {
+    console.log("Taobao USD Injected!! [s.taobao]")
+
+    $(window).on('load', function() {
+        //find all box
+        console.log($("[data-category='auctions']").length)
+        //resize all box to 420
+        $("[data-category='auctions']").css("height", "420px");
+        //clone the price tag
+        // console.log($("[data-category='auctions'] .price"))
+        // $("[data-category='auctions'] .row-1").before($("[data-category='auctions'] .price"))
+
+        $("[data-category='auctions']").each(function(index, each_element) {
+            var original_price = parseFloat($(this).find(".price>strong").get(0).innerHTML)
+            var usd_price = (original_price * currency_rate).toFixed(2)
+            //clone the price tag and style
+            //adjust margin for new price tag
+            //convert to html
+            var original_price_html = $(this)
+                .find(".price")
+                .clone()
+                .append(`<span style="margin: 0px;"> or </span><strong>`+usd_price+" USD</strong>")
+                .css({
+                    "margin-top": "10px",
+                    "margin-left": "10px",
+                    "font-size": "15px"
+                })
+                .get(0)
+                .outerHTML
+            //change the information box margin, prevent the intent of price tag
+            $(this).find(".price").remove()
+            $(this).find(".ctx-box").css({
+                "margin-top": "20px"
+            })
+            //add the cloned price tag under the picture
+            $(this).find(".ctx-box").before(original_price_html + "<br>")
+        });
+    })
+}
