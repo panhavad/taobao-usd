45480d515091adbfdb37c43fbe5dcbd1a4e6c065
panhavad <panhavadd@gmail.com>
2021-04-21 01:02:19 +0700

    handle J_shoper side

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 //cariable declarion
 var currency_url = "https://free.currconv.com/api/v7/convert?q=CNY_USD&compact=ultra&apiKey=6794cace11c7c0ba19d5"
-var currency_rate = 0
+var currency_rate = 0.15335 //pre-define incase the rate api dose not work will not big issue
 var warning_header = `
 <div style="
     padding: 5px;
@@ -55,9 +55,14 @@ if (location.href.includes("https://s.taobao.com/")) {
 
         //resize all box to 420
         $("[data-category='auctions']").css("height", "420px");
+        $("li.oneline").css('height', '320px')
+        console.log($("li[class$='-item']"))
+        $("li[class$='-item']").css('height', '415px')
 
         //start the custom price tag
         c_price_tag_main($("[data-category='auctions']"))
+        c_price_tag_ads_side($("#J_shopkeeper [class$='-line1']"), 1)//element, tag location(U=1/D=0)
+        // c_price_tag_ads_side($("#J_shopkeeper_bottom [class$='-line1']"), 0)//element, tag location(U=1/D=0) //checking
 
     })
 
@@ -89,15 +94,38 @@ if (location.href.includes("https://s.taobao.com/")) {
         load();
     })
 
-/**
-* @todo Handle J_shopkeeper price tag [s.taobao.com]
-* @body The advertisement items price tag on the side of main
-*/
+    /**
+     * @todo Handle J_shopkeeper price tag [s.taobao.com]
+     * @body The advertisement items price tag on the side of main
+     */
+    function c_price_tag_ads_side(ads_price, tag_location) {
+        //select all the price tag j shoper item list
+        console.log(ads_price)
+        ads_price.each(function(index, each_ad_price_box) {
+            var ad_price_sec = $(this).find("[class$='-price']") //get the price tag
+            var ad_original_price = parseFloat(ad_price_sec.get(0).outerText.substring(1)) //get the cny price
+            var ad_usd_price = (ad_original_price * currency_rate).toFixed(2) //calculate the usd price
+            var new_price_tag_html = ad_price_sec.clone().addClass("this_is_ads_custom_price_tag").append(" or " + ad_usd_price + " USD").css({
+                'font-size': '15px',
+                'margin-left': '10px'
+            }).get(0).outerHTML //working on adding usd price with old price
+            ad_price_sec.remove() //remove the old price tag
+            if (tag_location==1) {
+             $(this).before(new_price_tag_html + "<br>") //show the new price after the old price box
+            }else{
+             $(this).after(new_price_tag_html) //show the new price after the old price box
+            }
+        })
+        //check on class name that contant the -price
 
-/**
-* @todo Handle J_shopkeeper_buttom price tag [s.taobao.com]
-* @body The advertisement items price tag on the buttom of main
-*/
+        //change price tag with usd price and make it smaller
+
+    }
+
+    /**
+     * @todo Handle J_shopkeeper_buttom price tag [s.taobao.com]
+     * @body The advertisement items price tag on the buttom of main
+     */
 
 
     function c_price_tag_main(item_boxs) {
@@ -132,11 +160,11 @@ if (location.href.includes("https://s.taobao.com/")) {
 }
 
 /**
-* @todo Handle detail.tmall.com price tag
-* @body The items price tag on the detial item in tmall
-*/
+ * @todo Handle detail.tmall.com price tag
+ * @body The items price tag on the detial item in tmall
+ */
 
 /**
-* @todo Handle item.taobao.com price tag
-* @body The items price tag on the detial item in noraml taobao
-*/
+ * @todo Handle item.taobao.com price tag
+ * @body The items price tag on the detial item in noraml taobao
+ */
