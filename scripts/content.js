8429cb706d3dc01070db68a294d22bf980aab714
panhavad <panhavadd@gmail.com>
2021-04-21 21:56:05 +0700

    Fix issues on change page multiple time & improvement😎😎

--- scripts/content.js
+++ scripts/content.js
@@ -50,85 +50,53 @@ if (location.href.includes("https://s.taobao.com/")) {
 
     //when the page is loaded
     $(window).on('load', function() {
-        //find all box
-        // console.log($("[data-category='auctions']").length)
-
-        //resize all box to 420
-        $("[data-category='auctions']").css("height", "420px");
-        $("li.oneline").css('height', '320px')
-        console.log($("li[class$='-item']"))
-        $("li[class$='-item']").css('height', '415px')
-
         //start the custom price tag
         c_price_tag_main($("[data-category='auctions']"))
         c_price_tag_ads_side($("#J_shopkeeper [class$='-line1']"), 1)//element, tag location(U=1/D=0)
         // c_price_tag_ads_side($("#J_shopkeeper_bottom [class$='-line1']"), 0)//element, tag location(U=1/D=0) //checking
+        // c_price_initem
 
     })
 
+    //activate the page change recheck
+    click_event_reinitiate()
 
-    //purpose to detect of the user change items page
-    jQuery('*:not(.nocapture)').on('click', function(e) {
-        //detecting user click
-        //compare the current and previous url for 5 times loop of 1second
-        //if the url different recall the price tag replacement
-        //else add new price tag again
-
-        //compare the current and previous url for 5 times loop of 1second
-        // Returns a Promise that resolves after "ms" Milliseconds
-        const timer = ms => new Promise(res => setTimeout(res, ms))
-
-        async function load() { // We need to wrap the loop into an async function for this to work
-            for (var i = 0; i < 5; i++) {
-                //check the existent of custom price tag
-                var cpt_checker = $(".this_is_custom_price_tag").length
-                // console.log(cpt_checker)
-                //if no
-                if (cpt_checker == 0) {
-                    c_price_tag_main($("[data-category='auctions']"))
-                } //else just go to other loop
-                await timer(1000); // then the created Promise can be awaited
-            }
-        }
-
-        load();
-    })
-
-    /**
-     * @todo Handle J_shopkeeper price tag [s.taobao.com]
-     * @body The advertisement items price tag on the side of main
-     */
-    function c_price_tag_ads_side(ads_price, tag_location) {
-        //select all the price tag j shoper item list
-        console.log(ads_price)
-        ads_price.each(function(index, each_ad_price_box) {
-            var ad_price_sec = $(this).find("[class$='-price']") //get the price tag
-            var ad_original_price = parseFloat(ad_price_sec.get(0).outerText.substring(1)) //get the cny price
-            var ad_usd_price = (ad_original_price * currency_rate).toFixed(2) //calculate the usd price
-            var new_price_tag_html = ad_price_sec.clone().addClass("this_is_ads_custom_price_tag").append(" or " + ad_usd_price + " USD").css({
-                'font-size': '15px',
-                'margin-left': '10px'
-            }).get(0).outerHTML //working on adding usd price with old price
-            ad_price_sec.remove() //remove the old price tag
-            if (tag_location==1) {
-             $(this).before(new_price_tag_html + "<br>") //show the new price after the old price box
-            }else{
-             $(this).after(new_price_tag_html) //show the new price after the old price box
-            }
-        })
-        //check on class name that contant the -price
-
-        //change price tag with usd price and make it smaller
-
+    //this will handle on page change multiple time, so that the click event still available
+    function click_event_reinitiate() {
+      $("#mainsrp-pager .inner").on('click', function(e) {
+        recheck_on_pagechange()
+      })
     }
 
-    /**
-     * @todo Handle J_shopkeeper_buttom price tag [s.taobao.com]
-     * @body The advertisement items price tag on the buttom of main
-     */
+    function recheck_on_pagechange() {
+          //compare the current and previous url for 5 times loop of 1second
+          // Returns a Promise that resolves after "ms" Milliseconds
+          const timer = ms => new Promise(res => setTimeout(res, ms))
 
+          async function load() { // We need to wrap the loop into an async function for this to work
+              for (var i = 0; i < 5; i++) {
+                  //check the existent of custom price tag
+                  var cpt_checker = $(".this_is_custom_price_tag").length
+                  console.log(cpt_checker)
+                  //if no
+                  if (cpt_checker == 0) {
+                      c_price_tag_main($("[data-category='auctions']"))
+                      await timer(500);
+                      c_price_tag_ads_side($("#J_shopkeeper [class$='-line1']"), 1)//element, tag location(U=1/D=0)
+                      click_event_reinitiate()
+                      break
+                  } //else just go to other loop
+                  await timer(1000); // then the created Promise can be awaited
+              }
+          }
 
+          load();
+    }
+
+    //change price tag for the main page content middle
     function c_price_tag_main(item_boxs) {
+        //resize all box to 420
+        item_boxs.css("height", "420px");
         //in each box
         item_boxs.each(function(index, each_element) {
             var original_price = parseFloat($(this).find(".price>strong").get(0).innerHTML)
@@ -157,6 +125,37 @@ if (location.href.includes("https://s.taobao.com/")) {
             $(this).find(".ctx-box").before(original_price_html + "<br>")
         });
     }
+
+    //change price tag for side bar ads
+    function c_price_tag_ads_side(ads_price, tag_location) {
+        $("li.oneline").css('height', '320px')
+        //select all the price tag j shoper item list
+        console.log(ads_price)
+        ads_price.each(function(index, each_ad_price_box) {
+            var ad_price_sec = $(this).find("[class$='-price']") //get the price tag
+            var ad_original_price = parseFloat(ad_price_sec.get(0).outerText.substring(1)) //get the cny price
+            var ad_usd_price = (ad_original_price * currency_rate).toFixed(2) //calculate the usd price
+            var new_price_tag_html = ad_price_sec.clone().addClass("this_is_ads_custom_price_tag").append(" or " + ad_usd_price + " USD").css({
+                'font-size': '15px',
+                'margin-left': '10px'
+            }).get(0).outerHTML //working on adding usd price with old price
+            ad_price_sec.remove() //remove the old price tag
+            if (tag_location==1) {
+             $(this).before(new_price_tag_html + "<br>") //show the new price after the old price box
+            }else{
+             $(this).after(new_price_tag_html) //show the new price after the old price box
+            }
+        })
+        //check on class name that contant the -price
+
+        //change price tag with usd price and make it smaller
+
+    }
+
+    /**
+     * @todo Handle J_shopkeeper_buttom price tag [s.taobao.com]
+     * @body The advertisement items price tag on the buttom of main
+     */
 }
 
 /**
@@ -164,7 +163,4 @@ if (location.href.includes("https://s.taobao.com/")) {
  * @body The items price tag on the detial item in tmall
  */
 
-/**
- * @todo Handle item.taobao.com price tag
- * @body The items price tag on the detial item in noraml taobao
- */
+
